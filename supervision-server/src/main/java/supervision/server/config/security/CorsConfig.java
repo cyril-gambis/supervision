5f831c2e5a9143aff04f171491bd808d7a22ebd1
cyril_gambis <cyril.gambis@augeo.org>
2018-05-07 11:15:32 +0200

    Update maven compiler plugin version in pom

--- supervision-server/src/main/java/supervision/server/config/security/CorsConfig.java
+++ supervision-server/src/main/java/supervision/server/config/security/CorsConfig.java
@@ -9,6 +9,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 
+/**
+ * Méthode non standard Spring de gestion CORS car le filtre Spring Security se fait
+ * avant le filtre standard CORS de SecurityConfig.configure
+ * 
+ * @date 07/05/2018
+ * @author Cyril Gambis
+ */
 @Configuration
 public class CorsConfig {
 //IMPORTANT: it has to be a normal configuration class, 
@@ -28,3 +35,16 @@ public class CorsConfig {
         return bean;
     }
 }
+
+/*
+Autre possibilité, à tester éventuellement (mais à priori, cela ne fonctionne pas, notamment du fait des request preflight OPTION):
+
+https://docs.spring.io/spring-security/site/docs/current/reference/html/cors.html
+
+set your own CorsConfigurationSource in your Spring Security Config.
+e.g. @Override protected void configure(HttpSecurity http) throws Exception { http.cors().configurationSource(new CustomConfigSource()); }
+
+The CorsConfigurationSource offers a method CorsConfiguration
+getCorsConfiguration(HttpServletRequest request); in which you can handle your specific CORS Handling.
+
+*/
