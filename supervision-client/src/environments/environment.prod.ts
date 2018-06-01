de734dbc4ce38378bb446297303dbce159a0374c
cyril_gambis <cyril.gambis@augeo.org>
2018-06-01 11:38:56 +0200

    Added Virsna's prod parameters and sh scripts

--- supervision-client/src/environments/environment.prod.ts
+++ supervision-client/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  authorizationEndpoint: 'http://PROD_AUTHORIZATION:8091/api/v1.0',
-  resourceEndpoint: 'http://PROD_RESOURCE:8091/api/v1.0',
+  authorizationEndpoint: 'https://supervision-server.planzone.com/api/v1.0',
+  resourceEndpoint: 'https://supervision-server.planzone.com/api/v1.0',
+  dataEndpoint: 'https://supervision-data.planzone.com/api/v1.0',
   environmentName: 'Production Environment'
 };
